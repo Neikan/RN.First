a597028637dd272382945158e342d0265ded35ec
Neikan <sladkov.e.m@outlook.com>
2021-11-24 21:57:41 +0300

    feat(TodoScreen): add new components

--- src/screens/TodoScreen/index.tsx
+++ src/screens/TodoScreen/index.tsx
@@ -1,12 +1,19 @@
-import React, { FC } from 'react'
-import { Button, StyleSheet, Text, View } from 'react-native'
+import React, { FC, useState } from 'react'
+import { Button, StyleSheet, View } from 'react-native'
 
 import { DefaultTheme } from '@/consts/theme'
 
+import { TodoCard } from '@/components/TodoCard'
+
 import { ITodoScreenProps as IProps } from './types'
+import { EditTodo } from '@/components/EditTodo'
 
 const styles = StyleSheet.create({
-  screen: {},
+  screen: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'space-between'
+  },
   buttons: {
     flexDirection: 'row',
     justifyContent: 'space-between'
@@ -16,17 +23,25 @@ const styles = StyleSheet.create({
   }
 })
 
-export const TodoScreen: FC<IProps> = ({ onGoBack, todo }) => {
+export const TodoScreen: FC<IProps> = ({ onEditTodo, onGoBack, onRemoveTodo, todo }) => {
+  const [isModal, setIsModal] = useState(false)
+
+  const handleSave = (title: string): void => {
+    onEditTodo(todo.id, title)
+    setIsModal(false)
+  }
+
   return (
     <View style={styles.screen}>
-      <Text>{todo.title}</Text>
+      <EditTodo isVisible={isModal} todoTitle={todo.title} onCancel={() => setIsModal(false)} onSave={handleSave} />
+      <TodoCard todo={todo} onEdit={() => setIsModal(true)} />
 
       <View style={styles.buttons}>
         <View style={styles.button}>
-          <Button title='Назад' onPress={onGoBack} color={DefaultTheme.BRAND_500} />
+          <Button title='Назад' onPress={onGoBack} color={DefaultTheme.GRAY_500} />
         </View>
         <View style={styles.button}>
-          <Button title='Удалить' onPress={() => null} color={DefaultTheme.DANGER_500} />
+          <Button title='Удалить' onPress={() => onRemoveTodo(todo.id)} color={DefaultTheme.DANGER_500} />
         </View>
       </View>
     </View>
--- src/screens/TodoScreen/types.ts
+++ src/screens/TodoScreen/types.ts
@@ -3,4 +3,6 @@ import { ITodo } from 'components/Todo/types'
 export interface ITodoScreenProps {
   todo: ITodo
   onGoBack: () => void
+  onEditTodo: (id: string, title: string) => void
+  onRemoveTodo: (id: string) => void
 }
