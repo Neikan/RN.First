86167f151708ea739f83fbdc8f09fd4d57e317ca
Neikan <sladkov.e.m@outlook.com>
2021-11-24 18:38:37 +0300

    feat(Header): refactor component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,26 @@
+import React, { FC } from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+import { DefaultTheme } from 'consts/theme'
+
+import { IHeaderProps as IProps } from './types'
+
+const styles = StyleSheet.create({
+  view: {
+    height: 70,
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    backgroundColor: DefaultTheme.BRAND_500,
+    paddingBottom: 10
+  },
+  text: {
+    color: DefaultTheme.LIGHT,
+    fontSize: 20
+  }
+})
+
+export const Header: FC<IProps> = ({ title }) => (
+  <View style={styles.view}>
+    <Text style={styles.text}>{title}</Text>
+  </View>
+)
