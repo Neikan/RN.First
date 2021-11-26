ec6c0703ad2cb134bfb536adaf918788f23d9f5c
Neikan <sladkov.e.m@outlook.com>
2021-11-26 19:55:50 +0300

    feat(TodoScreen): refactor component

--- src/screens/TodoScreen/index.tsx
+++ src/screens/TodoScreen/index.tsx
@@ -1,14 +1,15 @@
-import React, { FC, useState } from 'react'
+import React, { FC, useContext, useState } from 'react'
 import { Dimensions, StyleSheet, View } from 'react-native'
 import { AntDesign, FontAwesome } from '@expo/vector-icons'
+
+import { ScreenContext } from '@/context/screen/screenContext'
+import { TodoContext } from '@/context/todo/todoContext'
 
 import { DefaultTheme } from '@/consts/theme'
 
 import { AppButton } from '@/components/AppButton'
 import { TodoCard } from '@/components/TodoCard'
 import { EditTodo } from '@/components/EditTodo'
-
-import { ITodoScreenProps as IProps } from './types'
 
 const styles = StyleSheet.create({
   screen: {
@@ -26,31 +27,45 @@ const styles = StyleSheet.create({
   }
 })
 
-export const TodoScreen: FC<IProps> = ({ onEditTodo, onGoBack, onRemoveTodo, todo }) => {
-  const [isModal, setIsModal] = useState(false)
+export const TodoScreen: FC = () => {
+  const { todos, removeTodo, updateTodo } = useContext(TodoContext)
+  const { todoId, changeScreen } = useContext(ScreenContext)
 
-  const handleSave = (title: string): void => {
-    onEditTodo(todo.id, title)
-    setIsModal(false)
-  }
+  const selectedTodo = todos.find((todo) => todo.id === todoId)
 
-  return (
-    <View style={styles.screen}>
-      <EditTodo isVisible={isModal} todoTitle={todo.title} onCancel={() => setIsModal(false)} onSave={handleSave} />
-      <TodoCard todo={todo} onEdit={() => setIsModal(true)} />
+  if (selectedTodo) {
+    const [isModal, setIsModal] = useState(false)
 
-      <View style={styles.buttons}>
-        <View style={styles.button}>
-          <AppButton onPress={onGoBack} backgroundColor={DefaultTheme.GRAY_500}>
-            <AntDesign name='back' size={24} />
-          </AppButton>
-        </View>
-        <View style={styles.button}>
-          <AppButton onPress={() => onRemoveTodo(todo.id)} backgroundColor={DefaultTheme.DANGER_500}>
-            <FontAwesome name='remove' size={24} />
-          </AppButton>
+    const handleSave = (title: string): void => {
+      updateTodo(selectedTodo.id, title)
+      setIsModal(false)
+    }
+
+    return (
+      <View style={styles.screen}>
+        <EditTodo
+          isVisible={isModal}
+          todoTitle={selectedTodo.title}
+          onCancel={() => setIsModal(false)}
+          onSave={handleSave}
+        />
+        <TodoCard todo={selectedTodo} onEdit={() => setIsModal(true)} />
+
+        <View style={styles.buttons}>
+          <View style={styles.button}>
+            <AppButton onPress={() => changeScreen(null)} backgroundColor={DefaultTheme.GRAY_500}>
+              <AntDesign name='back' size={24} />
+            </AppButton>
+          </View>
+          <View style={styles.button}>
+            <AppButton onPress={() => removeTodo(selectedTodo.id)} backgroundColor={DefaultTheme.DANGER_500}>
+              <FontAwesome name='remove' size={24} />
+            </AppButton>
+          </View>
         </View>
       </View>
-    </View>
-  )
+    )
+  }
+
+  return null
 }
--- src/screens/TodoScreen/types.ts
+++
@@ -1,8 +0,0 @@
-import { ITodo } from 'components/Todo/types'
-
-export interface ITodoScreenProps {
-  todo: ITodo
-  onGoBack: () => void
-  onEditTodo: (id: string, title: string) => void
-  onRemoveTodo: (id: string) => void
-}
