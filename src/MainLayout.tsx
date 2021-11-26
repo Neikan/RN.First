999c6dc375b5fcf35f7dbee9a5260d4b9352aee8
Neikan <sladkov.e.m@outlook.com>
2021-11-26 19:56:08 +0300

    feat(MainLayout): refactor component

--- src/MainLayout.tsx
+++ src/MainLayout.tsx
@@ -1,7 +1,7 @@
-import React, { FC, useContext, useState } from 'react'
+import React, { FC, useContext } from 'react'
 import { StyleSheet, View } from 'react-native'
 
-import { TodoContext } from '@/context/todo/todoContext'
+import { ScreenContext } from '@/context/screen/screenContext'
 
 import { Spacing } from '@/consts/theme'
 
@@ -9,55 +9,14 @@ import { Header } from '@/components/Header'
 import { MainScreen } from '@/screens/MainScreen'
 import { TodoScreen } from '@/screens/TodoScreen'
 
-import { Nullable } from '@/types'
-
 export const MainLayout: FC = () => {
-  const { todos, addTodo, removeTodo, updateTodo } = useContext(TodoContext)
-
-  const [todoId, setTodoId] = useState<Nullable<string>>(null)
-
-  const openTodo = (todoId: string): void => {
-    setTodoId(todoId)
-  }
-
-  //   todoTitle &&
-  //     Alert.alert(
-  //       'Удаление элемента',
-  //       `Вы уверены, что хотите удалить ${todoTitle}?`,
-  //       [
-  //         {
-  //           text: 'Подтвердить',
-  //           onPress: () => {
-  //             setTodoId(null)
-  //             setTodos((prev) => prev.filter((todo) => todo.id !== todoId))
-  //           }
-  //         },
-  //         {
-  //           text: 'Отмена',
-  //           style: 'cancel'
-  //         }
-  //       ],
-  //       { cancelable: true }
-  //     )
-  // }
-
-  const goBack = (): void => setTodoId(null)
-
-  let content = <MainScreen onAddTodo={addTodo} onOpenTodo={openTodo} onRemoveTodo={removeTodo} todos={todos} />
-
-  if (todoId) {
-    const selectedTodo = todos.find((todo) => todo.id === todoId)
-
-    if (selectedTodo) {
-      content = <TodoScreen onGoBack={goBack} onRemoveTodo={removeTodo} todo={selectedTodo} onEditTodo={updateTodo} />
-    }
-  }
+  const { todoId } = useContext(ScreenContext)
 
   return (
     <View>
       <Header title='Todo App' />
 
-      <View style={styles.screen}>{content}</View>
+      <View style={styles.screen}>{todoId ? <TodoScreen /> : <MainScreen />}</View>
     </View>
   )
 }
