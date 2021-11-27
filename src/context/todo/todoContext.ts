85fa3b4c55924adee8ce3707aea0f9b79e8e6576
Neikan <sladkov.e.m@outlook.com>
2021-11-27 13:54:39 +0300

    feat(TodoState): add new actions

--- src/context/todo/todoContext.ts
+++ src/context/todo/todoContext.ts
@@ -4,6 +4,8 @@ import { ITodoContext as IContext } from './TodoState/types'
 
 export const TodoContext = createContext<IContext>({
   todos: [],
+  loading: false,
+  error: null,
   addTodo: () => null,
   removeTodo: () => null,
   updateTodo: () => null
--- src/context/todo/todoReducer.ts
+++ src/context/todo/todoReducer.ts
@@ -1,40 +1,61 @@
-import { ITodo } from '@/components/Todo/types'
-import { ITodoState as IState, TodoActionTypes, TTodoActions } from './TodoState/types'
+import {
+  IAddTodoAction,
+  IClearErrorAction,
+  IFetchTodosAction,
+  IHideLoaderAction,
+  IRemoveTodoAction,
+  IShowErrorAction,
+  IShowLoaderAction,
+  ITodoState as IState,
+  IUpdateTodoAction,
+  TodoActionTypes,
+  TTodoActions
+} from './TodoState/types'
 
 const handlers = {
-  [TodoActionTypes.ADD_TODO]: (state: IState, title: string) => ({
+  [TodoActionTypes.ADD_TODO]: (state: IState, { payload }: IAddTodoAction) => ({
     ...state,
     todos: [
       ...state.todos,
       {
-        id: Date.now().toString(),
-        title
+        id: payload.title,
+        title: payload.title
       }
     ]
   }),
 
-  [TodoActionTypes.REMOVE_TODO]: (state: IState, id: string) => ({
+  [TodoActionTypes.REMOVE_TODO]: (state: IState, { payload }: IRemoveTodoAction) => ({
     ...state,
-    todos: state.todos.filter((todo) => todo.id !== id)
+    todos: state.todos.filter((todo) => todo.id !== payload)
   }),
 
-  [TodoActionTypes.UPDATE_TODO]: (state: IState, { id, title }: ITodo) => ({
+  [TodoActionTypes.UPDATE_TODO]: (state: IState, { payload }: IUpdateTodoAction) => ({
     ...state,
     todos: state.todos.map((todo) => {
-      if (todo.id === id) {
-        todo.title = title
+      if (todo.id === payload.id) {
+        todo.title = payload.title
       }
 
       return todo
     })
   }),
 
+  [TodoActionTypes.SHOW_LOADER]: (state: IState, action: IShowLoaderAction) => ({ ...state, loading: true }),
+
+  [TodoActionTypes.HIDE_LOADER]: (state: IState, action: IHideLoaderAction) => ({ ...state, loading: false }),
+
+  [TodoActionTypes.SHOW_ERROR]: (state: IState, { payload }: IShowErrorAction) => ({ ...state, error: payload }),
+
+  [TodoActionTypes.CLEAR_ERROR]: (state: IState, action: IClearErrorAction) => ({ ...state, error: null }),
+
+  [TodoActionTypes.FETCH_TODOS]: (state: IState, { payload }: IFetchTodosAction) => ({ ...state, todos: payload }),
+
   DEFAULT: (state: IState) => state
 }
 
-export const todoReducer = (state: IState, { type, payload }: TTodoActions): IState => {
-  const handler = handlers[type] ?? handlers.DEFAULT
+export const todoReducer = (state: IState, action: TTodoActions): IState => {
+  const handler = handlers[action.type] ?? handlers.DEFAULT
 
   // избавиться от any
-  return handler(state, payload as any)
+  return handler(state, action as any)
 }
