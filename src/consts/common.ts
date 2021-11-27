4278889a00d7b9d93c6b6b485ddf4d8d10e870ae
Neikan <sladkov.e.m@outlook.com>
2021-11-27 13:54:05 +0300

    feat(consts): add common

---
+++ src/consts/common.ts
@@ -0,0 +1,8 @@
+export const API = 'https://rn-first-todo-default-rtdb.europe-west1.firebasedatabase.app/'
+
+export enum Method {
+  DELETE = 'DELETE',
+  GET = 'GET',
+  PATCH = 'PATCH',
+  POST = 'POST'
+}
